8658b095909110d74197f4bbae5372fa7ed8b772
Asmi-0123 <ashwika333@gmail.com>
2025-08-20 18:00:16 +0530

    Delete migrations/0001_slim_magneto.sql

--- migrations/0001_slim_magneto.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE `contact_settings`;--> statement-breakpoint
-DROP TABLE `faq_items`;--> statement-breakpoint
-DROP TABLE `site_content`;--> statement-breakpoint
-DROP TABLE `theme_settings`;
